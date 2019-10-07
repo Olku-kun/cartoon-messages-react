7581fa37f5b45cc2e5510fd33782db43e8ab5952
Olku-kun <kraoly@gmail.com>
2019-10-07 12:13:23 +0300

    creating and updating Tile Component

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -7,10 +7,10 @@ class Tile extends React.Component {
         super(props);
     }
     render() {
-       const templates =  this.props.templates;
-       const tiles = templates.map(tile => <Col key={tile.id} lg = "3" md = "6">)
+      const {templates} = this.props.templates
         return (
              <div>
+                 image
                  </div>
            
         )
