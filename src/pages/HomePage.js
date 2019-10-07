f7c8eeefb7f9767316d171a5199ea1f31a04559a
Olku-kun <kraoly@gmail.com>
2019-10-07 11:15:19 +0300

    start button added at homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,21 +1,23 @@
 import React from 'react';
 
-import {  Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
 class HomePage extends React.Component {
     render() {
         return (
-            
-                <Container>
-              <h1>Steps to create your cartoon message </h1>
-              <ul> 
-                  <li>Choose your favorite template from the <a href ="#/templates" target = "_blank">Template Gallery</a></li>
-                  <li>Enter your message to the text input field</li>
-                  <li>Save your new cartoon message image to your computer or/and add it to our <a href ="#/gallery" target = "_blank">User Gallery</a></li>
-              </ul>
 
-                </Container>
-           
+            <Container>
+                <h1>Steps to create your cartoon message </h1>
+                <ul>
+                    <li>Choose your favorite template from the <a href="#/templates" target="_blank">Template Gallery</a></li>
+                    <li>Enter your message to the text input field</li>
+                    <li>Save your new cartoon message image to your computer or/and add it to our <a href="#/gallery" target="_blank">User Gallery</a></li>
+                </ul>
+                <Button variant="primary" size="lg" active href="#/login">Start</Button>
+                  
+                
+            </Container>
+
         )
     }
 }
