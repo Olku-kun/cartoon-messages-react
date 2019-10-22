b5467f7f1175f471fa3a79dcbe877dff52ea300c
Olku-kun <kraoly@gmail.com>
2019-10-22 12:59:20 +0300

    createing  MyPicture component

---
+++ src/components/MyPicture.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class MyPicture extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        const picture = this.props.picture;
+        return(
+            <div  id={picture.title} className='m-2'>
+            <img src={picture.src} title={picture.title} alt={picture.alt} />
+        </div>
+        )
+    }
+}
+export default MyPicture
--- src/pages/UserGallery.js
+++ src/pages/UserGallery.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import jsonTemplates from '../data/templates';
 import {  Container } from 'react-bootstrap';
 
 class UserGallery extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            pictures: jsonTemplates
+        }
+    }
     render() {
+        const {pictures} = this.state;
+        // const pictures = pictures.map((pic, i) => <Col  key = {i} id={pic.title} lg="4" md="6" sm="1"><MyPicture picture={pic} /></Col>);
         return (
             
                 <Container className = "base">
