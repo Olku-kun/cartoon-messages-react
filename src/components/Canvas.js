e16f886c71de8bbd6c10232f39664080572f6398
Olku-kun <kraoly@gmail.com>
2019-10-22 10:46:41 +0300

    fixing problem with image downloading

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -16,18 +16,20 @@ class Canvas extends React.Component {
         console.log(this.props);
         const canvas = this.canvas.current
         const img = this.image.current;
+        // img.origin = 'anonymous';
+        // img.src="https://i.ibb.co/12WjwTf/mockup-test.png";
         const ctx = canvas.getContext("2d");
-        // src="https://i.ibb.co/12WjwTf/mockup-test.png";
+        
         ctx.drawImage(img, 0, 0);
         ctx.fillStyle = "tomato"
         ctx.font = "18px Mansalva"
 
         ctx.fillText(this.props.canvastext, 150, 50)
     }
-    downloadImg = (el)=> {
+    downloadImg = (e)=> {
         var canvas = this.canvas.current;
-        var image = canvas.toDataURL('image/png');
-        el.href = image;
+        var image = canvas.toDataURL('image/jpg');
+        e.target.href = image;
         console.log(image)
         
       }
@@ -41,7 +43,7 @@ class Canvas extends React.Component {
                 />
 
                 <img ref={this.image} src={this.props.imgSrc} style={{ display: 'none' }} />
-                <a download = "image.png" href="" onClick={this.downloadImg} crossOrigin="Anonymous">Download </a>
+                <a download = "image.jpg" href="" onClick={this.downloadImg} crossOrigin="anonymous">Download </a>
 
             </div>
 
