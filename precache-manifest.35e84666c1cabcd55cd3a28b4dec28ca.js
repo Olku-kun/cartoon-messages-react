8ff2fd6a9b9544a8b50d381ec139a55dbe38e69d
Olku-kun <kraoly@gmail.com>
2019-10-29 12:04:37 +0200

    Updates

---
+++ precache-manifest.35e84666c1cabcd55cd3a28b4dec28ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0646963ad16c5805a1055adc1ce90315",
+    "url": "/cartoon-messages-reactnpm%20install%20--save%20gh-pages/index.html"
+  },
+  {
+    "revision": "8699e67edcfda4ead6ce",
+    "url": "/cartoon-messages-reactnpm%20install%20--save%20gh-pages/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "02674fe2de729cdeae19",
+    "url": "/cartoon-messages-reactnpm%20install%20--save%20gh-pages/static/css/main.3f286ce6.chunk.css"
+  },
+  {
+    "revision": "8699e67edcfda4ead6ce",
+    "url": "/cartoon-messages-reactnpm%20install%20--save%20gh-pages/static/js/2.7e01663d.chunk.js"
+  },
+  {
+    "revision": "02674fe2de729cdeae19",
+    "url": "/cartoon-messages-reactnpm%20install%20--save%20gh-pages/static/js/main.b940be28.chunk.js"
+  },
+  {
+    "revision": "e1813b884ba534d902ac",
+    "url": "/cartoon-messages-reactnpm%20install%20--save%20gh-pages/static/js/runtime-main.4e2ba823.js"
+  }
+]);
